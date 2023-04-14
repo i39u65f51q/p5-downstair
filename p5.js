c4d0ecb971b7f75255082d2e0d56b0e508ed3b59
Alan <i39u65f51q@gmail.com>
2023-04-14 12:56:03 +0800

    feat(all): first version

--- p5.js
+++ p5.js
@@ -1,21 +1,150 @@
-/* Width, Height */
-const screenWidth = document.querySelector('#container').clientWidth;
-const screenHeight = document.querySelector('#container').clientHeight;
-/* Score DOM */
+/* Media-Screen Width, Height */
+const container = document.querySelector('#container');
+const screenWidth = container.clientWidth;
+const screenHeight = Math.floor(container.clientHeight / 10) * 10;
+
 const scoreElement = document.querySelector('.score');
-/* Heart DOM */
 const heartElement = document.querySelector('.heart');
-/* Global Value */
+const gameStatusElement = document.querySelector('.game-status');
+/* ----------- Global Value ------------*/
 let score = 0;
-let heart = 5;
+let heart = 3;
 
-const bgColor = 220;
-let x = 10;
-let y = 10;
+let isEndGame = false;
 
-window.addEventListener('load', initializeGame);
+/* 角色設定 */
+let roleImg;
+let roleX = screenWidth / 2;
+let roleY = 50;
+let step = 0;
+let direction = 1; //1=LEFT, 2=RIGHT
+let isStandOnBuilding = false;
 
+/* 建築設定 */
+const bWidth = 250;
+const bHeight = 20;
+let standIndex = -1;
+const buildings = [{ x: screenWidth / 2 - 100, y: 700, width: bWidth, height: bHeight }];
+
+/* ----------- Processing ------------*/
+function setup() {
+  createCanvas(screenWidth, screenHeight); //size
+  roleImg = loadImage('./asset/role.png');
+}
+
+//每幀(0.01s)執行一次
+function draw() {
+  if (heart === 0) {
+    endGame();
+    return;
+  }
+  /* 角色超出螢幕範圍 */
+  if (roleY >= screenHeight || roleY <= 0) {
+    roleDead();
+  }
+  /* 增除上一幀畫面 */
+  clear();
+
+  /* 建立樓梯物件 */
+  buildings.forEach(b => {
+    fill(250);
+    stroke(0);
+    rect(b.x, b.y, b.width, b.height);
+  });
+  buildings.forEach(b => {
+    score <= 20 ? b.y-- : score <= 40 ? (b.y -= 2) : (b.y -= 4);
+  });
+
+  /* 清除超出螢幕外的物件 */
+  // const removeIndex = buildings.findIndex(b => b.y <= 10);
+  // removeIndex !== -1 && buildings.splice(removeIndex, 1);
+
+  /* 掉落：如果role在螢幕範圍內 && 未站在物件上 */
+  if (roleY < screenHeight && !isStandOnBuilding) {
+    fallDown();
+  }
+  /* 判斷人物是否站在物件上 */
+  const checkStandBuildingIndex = buildings.findIndex(
+    b => roleY === b.y - bHeight * 2 && roleX >= b.x && roleX <= b.x + bWidth,
+  );
+  if (checkStandBuildingIndex !== -1 && standIndex === -1) {
+    standIndex = checkStandBuildingIndex;
+    isStandOnBuilding = true;
+  }
+  if (standIndex !== -1) {
+    roleY = buildings[standIndex].y - bHeight * 2;
+
+    /* 判斷角色是否站在建築物上 */
+    if (roleX + 32 <= buildings[standIndex].x || roleX >= buildings[standIndex].x + bWidth) {
+      isStandOnBuilding = false;
+      standIndex = -1;
+    }
+  }
+  /* 建立角色 */
+  role();
+
+  /* 鍵盤事件 */
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW)) {
+    step++;
+    step = step % 4;
+  }
+  if (keyIsDown(LEFT_ARROW) && isStandOnBuilding) toLeft();
+  if (keyIsDown(RIGHT_ARROW) && isStandOnBuilding) toRight();
+
+  /* 每60幀=1S */
+  if (!isEndGame && frameCount % 60 === 0) {
+    gBuilding();
+    score++;
+    scoreElement.textContent = score;
+  }
+}
+function role() {
+  //p5: image(img, dx, dy, dWidth, dHeight, sx, sy, sWidth, sHeight, fit, xAlign, yAlign)
+  image(roleImg, roleX, roleY, 32, 48, step * 32, direction * 48, 32, 48, step * 32 + 32, direction * 48);
+}
+function toLeft() {
+  direction = 1;
+  roleX -= 5;
+}
+/* px= 128, py=96 */
+function toRight() {
+  direction = 2;
+  roleX += 5;
+}
+
+function fallDown() {
+  score <= 20 ? roleY++ : score <= 40 ? (role += 2) : (role += 4);
+}
+/* 角色受傷 */
+function roleDead() {
+  if (heart === 0) {
+    endGame();
+    return;
+  }
+  heart--;
+  loadHeart();
+  resetRole();
+}
+/* 受傷後重設角色位置 */
+function resetRole() {
+  roleX = screenWidth / 2;
+  roleY = 50;
+  standIndex = -1;
+  isStandOnBuilding = false;
+}
+/* 新增物件 */
+function gBuilding() {
+  const x = Math.ceil(Math.random() * 1000) + 10;
+  const y = Math.floor((screenHeight - Math.random() * 15) / 100) * 100;
+  buildings.push({ x: x, y: y, width: bWidth, height: bHeight });
+}
+/* ----------- 遊戲初始化 ------------*/
+function initializeGame() {
+  loadHeart();
+  loadScore();
+}
 function loadHeart() {
+  heartElement.innerHTML = '';
   for (let i = 1; i <= heart; i++) {
     const heartImg = document.createElement('img');
     heartImg.src = './asset/heart.png';
@@ -26,14 +155,10 @@ function loadHeart() {
 function loadScore() {
   scoreElement.textContent = score;
 }
-function initializeGame() {
-  loadHeart();
-  loadScore();
+/* TODO: 結束遊戲 */
+function endGame() {
+  isEndGame = true;
 }
-/* Processing  */
-function setup() {
-  createCanvas(screenWidth, screenHeight);
-}
-function draw() {
-  text(a, screenWidth / 2, screenHeight / 2);
-}
+
+// window.addEventListener('click', startGame);
+window.addEventListener('load', initializeGame);
